bd98fd309b6b8deb102054cde526f1b3bf6a629f
Henry Chen <henrychen385@gmail.com>
2018-12-17 12:17:40 +0800

    added jwt to order post

--- routes/order.js
+++ routes/order.js
@@ -9,17 +9,24 @@ router.get('/',(req,res,next) => {
     res.json({message:'Welcome to the Orders API'})
 })
 
-router.post('/', (req,res,next) => {
-    const new_order = new Order ({
-        _id: new mongoose.Types.ObjectId,
-        ...req.body
-    })
-    new_order.save()
-        .then(message => {
-            res.json(message)
-            console.log(new_order)
+router.post('/', jwtMiddleware.decode(), (req,res,next) => {
+    const token = req.verification ? req.verification : null
+    
+    if(token) {
+        const new_order = new Order ({
+            _id: new mongoose.Types.ObjectId,
+            ...req.body
         })
-        .catch(error => {error:error.message})
+        
+        new_order.save()
+            .then(message => {
+                res.json(message)
+                console.log(new_order)
+            })
+            .catch(error => {error:error.message})
+    } else {
+        res.json({error:'invalid permissions'})
+    }
 })
 
 router.get('/allOrders', jwtMiddleware.decode(), (req,res,next) => {
